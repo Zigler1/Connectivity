1b48c61e0435618e696359ec4dad22bf0777d1fb
anwar zigler <anouar2002zigler@gmail.com>
2021-01-15 21:05:36 +0100

    Add project files.

---
+++ Connectivity/Connectivity/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+
+namespace Connectivity
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            CrossConnectivity.Current.ConnectivityChanged += connectivityEvent;
+        }
+
+        private void connectivityEvent(object sender, ConnectivityChangedEventArgs e)
+        {
+            //when app detect internet
+            if(e.IsConnected)
+            {
+                MainPage = new MainPage();
+            }
+            //when app detect no internet
+            if(!e.IsConnected)
+            {
+                MainPage = new MainPage();
+            }
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
